b2d7e122a018ed10146e8420aab461c21521795b
ambarbidkar-creator <ambar.bidkar@xecurify.com>
2026-06-26 17:04:22 +0530

    Update customjs.js

--- customjs.js
+++ customjs.js
@@ -572,15 +572,15 @@
     /* Server-rendered error banner -> show below the password field */
     var isPageHasError = errorOnPage();
     if (isPageHasError) {
-      console.log('IN ERROR SECTION ');
-      console.log('IN ERROR SECTION ')
-      var message = $('#error-alert-message .errorMessage li span').text().trim();
-      $('#username').after(
-        '<div class="error-message text-start" style="color:red;">' + message + '</div>'
-      );
-      $('input').addClass('border border-danger');
-      $('.username').addClass('border border-danger');
-      $('#error-alert-message').hide();
+      // console.log('IN ERROR SECTION ');
+      // console.log('IN ERROR SECTION ')
+      // var message = $('#error-alert-message .errorMessage li span').text().trim();
+      // $('#username').after(
+      //   '<div class="error-message text-start" style="color:red;">' + message + '</div>'
+      // );
+      // $('input').addClass('border border-danger');
+      // $('.username').addClass('border border-danger');
+      // $('#error-alert-message').hide();
       
     }
 
