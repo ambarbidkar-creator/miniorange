e044c31cc50cd0be6ffc0c4a8b4e8c3e11c66c1a
ambarbidkar-creator <ambar.bidkar@xecurify.com>
2026-06-28 11:39:19 +0530

    Update customjs.js

--- customjs.js
+++ customjs.js
@@ -1432,6 +1432,15 @@
         otpIcon.innerHTML = '<svg viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"/></svg>';
         otpWrap.appendChild(otpIcon);
       }
+      /* Clear the error indicators once the user edits the OTP again */
+      if (otpInput && !otpInput.dataset.moOtpClear) {
+        otpInput.dataset.moOtpClear = "true";
+        otpInput.addEventListener("input", function () {
+          $('#mo-userlogin-error').remove();
+          $('#mo-otp-icon').remove();
+          $(this).removeClass('border border-danger mo-input-error');
+        });
+      }
       $('#error-alert-message').hide();
     }
 
